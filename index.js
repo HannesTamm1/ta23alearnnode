93c315791985d5cd701aa4e64fdf80adde19dd87
Hannes Tamm <hannes.tamm@tptlive.ee>
2024-10-21 12:52:34 +0300

    es vs commonjs

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-const fs = require('fs');
-fs.writeFileSync('test.txt', 'Hello node!');
+//const fs = require('fs'); // commonjs module
+//import fs from 'fs'; // es module
+//fs.writeFileSync('test.txt', 'Hello node!');
+const common = require('./common.js');
+common.hello();
+import es from './es.js';
+es.hello();
