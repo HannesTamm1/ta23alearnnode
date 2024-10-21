dfb9bb9a907a11af3f7d2111ac9364c66bc611f2
Hannes Tamm <hannes.tamm@tptlive.ee>
2024-10-21 13:25:25 +0300

    Add boxen

--- index.js
+++
@@ -1,7 +0,0 @@
-//const fs = require('fs'); // commonjs module
-//import fs from 'fs'; // es module
-//fs.writeFileSync('test.txt', 'Hello node!');
-const common = require('./common.js');
-common.hello();
-const es = require('./es.js').default;
-es.hello();
