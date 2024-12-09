156e1a7664b088f6db498672a42849838f4fadc5
Hannes Tamm <hannes.tamm@tptlive.ee>
2024-12-09 21:10:32 +0200

    add vue

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+    let message = 'Hello World!'
+</script>
+
+    
+
+<template>
+    <h1>{{ message }}</h1>
+</template>
+
+
+
+<style>
+
+</style>
--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import './style.scss';
-console.log("Hello Webpack!");
+
+import { createApp } from 'vue';
+
+
+import App from './App.vue';
+
+const app = createApp(App);
+app.mount('#app');
