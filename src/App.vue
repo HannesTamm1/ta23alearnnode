66753842cd4f7d0183a1e78ec5c3fa59e1de6c68
Hannes Tamm <hannes.tamm@tptlive.ee>
2025-01-14 11:37:52 +0200

    Shopping list

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,41 @@
 <script setup>
-    let message = 'Hello World!'
+import { ref } from 'vue';
+
+let message = ref('');
+let items = ref(['Piim', 'Krõpsud']);
+
+function addItem(){
+    if(message.value.trim() !== ''){
+        items.value.push(message.value.trim());
+    }
+    message.value = '';
+}
+
+
+
 </script>
 
-    
+
 
 <template>
-    <h1>{{ message }}</h1>
+    <div class="container">
+        <div class="field has-addons">
+            <div class="control">
+                <input class="input" type="text" v-model="message" @keypress.enter="addItem">
+            </div>
+            <div class="control">
+                <button class="button is-primary" @click="addItem">
+                    Add Item
+                </button>
+            </div>
+        </div>
+        <div class="content">
+            <h3>Shopping list:</h3>
+            <ul>
+                <li v-for="item in items">{{item}}</li>
+            </ul>
+        </div>
+    </div>
 </template>
 
-
-
-<style>
-
-</style>
+<style></style>
