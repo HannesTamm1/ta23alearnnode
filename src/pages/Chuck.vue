f0e84adc961be4a4b22d1655c5e5b70f75906803
Hannes Tamm <hannes.tamm@tptlive.ee>
2025-02-18 11:12:51 +0200

    asycn/suspense await example

--- src/pages/Chuck.vue
+++ src/pages/Chuck.vue
@@ -4,17 +4,10 @@ import { ref } from 'vue';
 
 let joke = ref('');
 
-function reqListener() {
-    	let data = JSON.parse(this.responseText)
-        console.log(data.value);
-        joke.value = data.value
-}
-
-const req = new XMLHttpRequest();
-req.addEventListener("load", reqListener);
-req.open("GET", "https://api.chucknorris.io/jokes/random");
-req.send();
-
+let response = await fetch('https://api.chucknorris.io/jokes/random');
+let data = await response.json();
+console.log(data);
+joke.value = data.value;
 </script>
 
 <template>
