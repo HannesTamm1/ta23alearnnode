d4e2b2f842d31001ce8aaa7c6aca6c10f6361bda
Hannes Tamm <hannes.tamm@tptlive.ee>
2025-03-11 11:45:50 +0200

    Add pagination

--- src/pages/RickAndMorty.vue
+++ src/pages/RickAndMorty.vue
@@ -3,11 +3,12 @@
 import axios from 'axios';
 import { ref } from 'vue';
 import CharacterCard from '../components/CharacterCard.vue';
+import SimplePagination from '../components/SimplePagination.vue';
+import Pagination from '../components/Pagination.vue';
 
 const characters = ref([]);
 const info = ref({});
-
-
+const currentPage = ref(1);
 await getCharacters('https://rickandmortyapi.com/api/character');
 
 
@@ -19,24 +20,19 @@ async function getCharacters(url) {
 }
 
 async function next(){
+    currentPage.value++;
     await getCharacters(info.value.next);
 }
 
 async function prev(){
+    currentPage.value--;
     await getCharacters(info.value.prev);
 }
 
 </script>
 
 <template>
-    <div class="buttons">
-        <button class="button is-primary" @click="prev" :disabled="!info.prev">
-            prev
-        </button>
-        <button class="button is-primary" @click="next" :disabled="!info.next">
-            next
-        </button>
-    </div>
+    <Pagination :info="info" :current="currentPage" @next="next" @prev="prev"></Pagination>
     <div class="columns is-multiline">
         <div v-for="character in characters" class="column is-one-quarter">
             <CharacterCard :character="character"></CharacterCard>
