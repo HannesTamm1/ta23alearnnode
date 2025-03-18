74fa568db5c9aec6fe8af52c30592bae95b197ef
Hannes Tamm <hannes.tamm@tptlive.ee>
2025-03-18 11:11:44 +0200

    Debounce search

--- src/pages/RickAndMorty.vue
+++ src/pages/RickAndMorty.vue
@@ -11,6 +11,7 @@ const info = ref({});
 const currentPage = ref(1);
 const searchValue = ref('');
 const error = ref('');
+let searchTimeout = null;
 
 await getCharacters('https://rickandmortyapi.com/api/character');
 
@@ -51,9 +52,12 @@ async function page(page) {
 }
 
 async function search() {
-    error.value = '';
-    currentPage.value = 1;
-    await getCharacters();
+    clearTimeout(searchTimeout);
+    searchTimeout = setTimeout(async () => {
+        error.value = '';
+        currentPage.value = 1;
+        await getCharacters();
+    },300);
 }
 
 </script>
@@ -62,7 +66,7 @@ async function search() {
 <template>
     <div class="field has-addons">
         <div class="control is-expanded">
-            <input v-model="searchValue" class="input" type="text" placeholder="Find character">
+            <input @input="search" v-model="searchValue" class="input" type="text" placeholder="Find character">
         </div>
         <div class="control">
             <button @click="search" class="button is-info">
