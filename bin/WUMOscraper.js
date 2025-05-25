dcfe962090d6ce9f5de34ac2ca7730e3f5218560
Hannes Tamm <hannes.tamm@tptlive.ee>
2025-05-25 20:33:28 +0300

    Add garfield and explosm scrapers

--- bin/WUMOscraper.js
+++ bin/WUMOscraper.js
@@ -1,45 +1,42 @@
 import axios from "axios";
-import * as cheerio from "cheerio";
-import fs from "fs";
+import * as cheerio from 'cheerio';
+import fs from 'fs';
 
 function cacheGet(name){
-    if(fs.existsSync(`../cache/${name}.html`)){
-    return fs.readFileSync(`../cache/${name}.html`);
+    if(fs.existsSync(`./cache/${name}.html`)) {
+        return fs.readFileSync(`./cache/${name}.html`);
     }
     return false;
 }
 
 function cacheSet(name, value){
-    if(!fs.existsSync(`../cache`)){
-        fs.mkdirSync(`../cache`);
+    if(!fs.existsSync('./cache')){
+        fs.mkdirSync('./cache');
     }
-    fs.writeFileSync(`../cache/${name}.html`, value);
+    fs.writeFileSync(`./cache/${name}.html`, value);
 }
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-let baseURL = 'https://wumo.com/';
-let url = baseURL + '';
+let baseUrl = 'https://wumo.com';
+let url = baseUrl + '';
 for(let i = 0; i<10;i++){
-let name = url.replaceAll('/', '').replaceAll(':', '');
+    let name = url.replaceAll('/', '').replaceAll(':', '');
     let data = cacheGet(name);
-    if(!data){
-        console.log('LIVEEE REQUESTT!!')
-        await sleep(1);
+    if(!data) {
+        console.log('LIVE REQUEST!!!!!!');
+        await sleep(1000);
         let res = await axios.get(url);
         data = res.data;
         cacheSet(name, data);
-    };
-        data = res.data;
-        cacheSet(name, data);
-
+    }
     //console.log(res.data);
-    const $ = cheerio.load(res.data);
-    let src = $('article.wumo img').attr('src');
-    let alt = $('article.wumo img').attr('alt');
+    const $ = cheerio.load(data);
+    let src = baseUrl + $('article.wumo img').attr('src');
+    let title = $('article.wumo img').attr('alt');
     console.log(src);
-    console.log("The alt:" + alt);
-    url = baseURL + $(`article.wumo a.prev`).attr('href');
+    console.log(title);
+    url = baseUrl + $('article.wumo a.prev').attr('href');
     console.log(url);
 }
